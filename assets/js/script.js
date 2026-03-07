027101330ca4129cde495b6dd37a53173b7ac819
Netylyov <ura88as88@gmail.com>
2026-03-07 10:14:48 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,6 @@ if (burgerBtn && mobileMenu) {
   burgerBtn.onclick = () => mobileMenu.classList.toggle('open');
 }
 
-/* ===========================
-   TOAST УВЕДОМЛЕНИЕ
-=========================== */
-function showToast(text) {
-  const toast = document.getElementById('toast');
-  toast.textContent = text;
-  toast.classList.add('show');
-
-  setTimeout(() => {
-    toast.classList.remove('show');
-  }, 1500);
-}
-
 
 /* ===========================
    ФИЛЬТР МЕНЮ
